9d0927e7a84921d67b812f7634bba1f3d8693ef3
Vinicius Rocha <vinicius.r.s@globo.com>
2021-03-30 14:00:37 -0300

    Initial commit

---
+++ Shared/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Shared
+//
+//  Created by Vinicius Rocha on 30/03/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
